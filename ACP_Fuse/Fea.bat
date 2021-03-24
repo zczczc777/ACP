6ab05815aae5ed6b3f84c746af523af7208a2eb0
zczczc777 <32671863+zczczc777@users.noreply.github.com>
2021-03-24 09:42:11 +0800

    Add files via upload

---
+++ ACP_Fuse/Fea.bat
@@ -0,0 +1,58 @@
+@echo off
+cd C:\Users\Administrator.OS-201709041456\Desktop\ACP_Fuse/
+python Fea.py --file ACP_mixed_test.fasta --type CKSAAP --out csv/CKSAAP.csv
+python Fea.py --file ACP_mixed_test.fasta --type CKSAAGP --out csv/CKSAAGP.csv
+python Fea.py --file ACP_mixed_test.fasta --type CTDC --out csv/CTDC.csv
+python Fea.py --file ACP_mixed_test.fasta --type CTDD --out csv/CTDD.csv
+python Fea.py --file ACP_mixed_test.fasta --type CTDT --out csv/CTDT.csv
+python Fea.py --file ACP_mixed_test.fasta --type CTriad --out csv/CTriad.csv
+python Fea.py --file ACP_mixed_test.fasta --type DDE --out csv/DDE.csv
+python Fea.py --file ACP_mixed_test.fasta --type DPC --out csv/DPC.csv
+python Fea.py --file ACP_mixed_test.fasta --type GAAC --out csv/GAAC.csv
+python Fea.py --file ACP_mixed_test.fasta --type GDPC --out csv/GDPC.csv
+python Fea.py --file ACP_mixed_test.fasta --type GTPC --out csv/GTPC.csv
+python Fea.py --file ACP_mixed_test.fasta --type KSCTriad --out csv/KSCTriad.csv
+python Fea.py --file ACP_mixed_test.fasta --type TPC --out csv/TPC.csv
+python Fea.py --file ACP_mixed_test.fasta --type ZSCALE --CutLength NT1 --out csv/ZSCALE(NT1).csv
+python Fea.py --file ACP_mixed_test.fasta --type ZSCALE --CutLength NT2 --out csv/ZSCALE(NT2).csv
+python Fea.py --file ACP_mixed_test.fasta --type ZSCALE --CutLength NT3 --out csv/ZSCALE(NT3).csv
+python Fea.py --file ACP_mixed_test.fasta --type ZSCALE --CutLength NT4 --out csv/ZSCALE(NT4).csv
+python Fea.py --file ACP_mixed_test.fasta --type ZSCALE --CutLength NT5 --out csv/ZSCALE(NT5).csv
+python Fea.py --file ACP_mixed_test.fasta --type ZSCALE --CutLength NT6 --out csv/ZSCALE(NT6).csv
+python Fea.py --file ACP_mixed_test.fasta --type ZSCALE --CutLength NT7 --out csv/ZSCALE(NT7).csv
+python Fea.py --file ACP_mixed_test.fasta --type ZSCALE --CutLength NT8 --out csv/ZSCALE(NT8).csv
+python Fea.py --file ACP_mixed_test.fasta --type ZSCALE --CutLength NT9 --out csv/ZSCALE(NT9).csv
+python Fea.py --file ACP_mixed_test.fasta --type ZSCALE --CutLength NT10 --out csv/ZSCALE(NT10).csv
+python Fea.py --file ACP_mixed_test.fasta --type BLOSUM62 --CutLength NT1 --out csv/BLOSUM62(NT1).csv
+python Fea.py --file ACP_mixed_test.fasta --type BLOSUM62 --CutLength NT2 --out csv/BLOSUM62(NT2).csv
+python Fea.py --file ACP_mixed_test.fasta --type BLOSUM62 --CutLength NT3 --out csv/BLOSUM62(NT3).csv
+python Fea.py --file ACP_mixed_test.fasta --type BLOSUM62 --CutLength NT4 --out csv/BLOSUM62(NT4).csv
+python Fea.py --file ACP_mixed_test.fasta --type BLOSUM62 --CutLength NT5 --out csv/BLOSUM62(NT5).csv
+python Fea.py --file ACP_mixed_test.fasta --type BLOSUM62 --CutLength NT6 --out csv/BLOSUM62(NT6).csv
+python Fea.py --file ACP_mixed_test.fasta --type BLOSUM62 --CutLength NT7 --out csv/BLOSUM62(NT7).csv
+python Fea.py --file ACP_mixed_test.fasta --type BLOSUM62 --CutLength NT8 --out csv/BLOSUM62(NT8).csv
+python Fea.py --file ACP_mixed_test.fasta --type BLOSUM62 --CutLength NT9 --out csv/BLOSUM62(NT9).csv
+python Fea.py --file ACP_mixed_test.fasta --type BLOSUM62 --CutLength NT10 --out csv/BLOSUM62(NT10).csv
+python Fea.py --file ACP_mixed_test.fasta --type AAINDEX --CutLength NT1 --out csv/AAINDEX(NT1).csv
+python Fea.py --file ACP_mixed_test.fasta --type AAINDEX --CutLength NT2 --out csv/AAINDEX(NT2).csv
+python Fea.py --file ACP_mixed_test.fasta --type AAINDEX --CutLength NT3 --out csv/AAINDEX(NT3).csv
+python Fea.py --file ACP_mixed_test.fasta --type AAINDEX --CutLength NT4 --out csv/AAINDEX(NT4).csv
+python Fea.py --file ACP_mixed_test.fasta --type AAINDEX --CutLength NT5 --out csv/AAINDEX(NT5).csv
+python Fea.py --file ACP_mixed_test.fasta --type AAINDEX --CutLength NT6 --out csv/AAINDEX(NT6).csv
+python Fea.py --file ACP_mixed_test.fasta --type AAINDEX --CutLength NT7 --out csv/AAINDEX(NT7).csv
+python Fea.py --file ACP_mixed_test.fasta --type AAINDEX --CutLength NT8 --out csv/AAINDEX(NT8).csv
+python Fea.py --file ACP_mixed_test.fasta --type AAINDEX --CutLength NT9 --out csv/AAINDEX(NT9).csv
+python Fea.py --file ACP_mixed_test.fasta --type AAINDEX --CutLength NT10 --out csv/AAINDEX(NT10).csv
+python Fea.py --file ACP_mixed_test.fasta --type EGAAC --CutLength NT5 --out csv/GAAC(NTCT5).csv
+python Fea.py --file ACP_mixed_test.fasta --type EGAAC --CutLength NT6 --out csv/GAAC(NTCT6).csv
+python Fea.py --file ACP_mixed_test.fasta --type EGAAC --CutLength NT7 --out csv/GAAC(NTCT7).csv
+python Fea.py --file ACP_mixed_test.fasta --type EGAAC --CutLength NT8 --out csv/GAAC(NTCT8).csv
+python Fea.py --file ACP_mixed_test.fasta --type EGAAC --CutLength NT9 --out csv/GAAC(NTCT9).csv
+python Fea.py --file ACP_mixed_test.fasta --type EGAAC --CutLength NT10 --out csv/GAAC(NTCT10).csv
+python Fea.py --file ACP_mixed_test.fasta --type EAAC --CutLength NT5 --out csv/AAC(NTCT5).csv
+python Fea.py --file ACP_mixed_test.fasta --type EAAC --CutLength NT6 --out csv/AAC(NTCT6).csv
+python Fea.py --file ACP_mixed_test.fasta --type EAAC --CutLength NT7 --out csv/AAC(NTCT7).csv
+python Fea.py --file ACP_mixed_test.fasta --type EAAC --CutLength NT8 --out csv/AAC(NTCT8).csv
+python Fea.py --file ACP_mixed_test.fasta --type EAAC --CutLength NT9 --out csv/AAC(NTCT9).csv
+python Fea.py --file ACP_mixed_test.fasta --type EAAC --CutLength NT10 --out csv/AAC(NTCT10).csv
+exit
--- ACP_Fuse/Fin.bat
+++ ACP_Fuse/Fin.bat
@@ -1,5 +1,5 @@
 @echo off
 c:
-cd C:\Users\Administrator\Desktop\ACP_Fuse//csv/
+cd C:\Users\Administrator.OS-201709041456\Desktop\ACP_Fuse//csv/
 python IndenpendenceTest.py RFFT174.model FT174.csv
 exit
